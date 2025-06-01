99e69635a377bf19e98b615372bac75c1d0c3227
Apirak Kanuengkarn <brkk_3804@hotmail.com>
2025-06-01 13:25:52 +0700

    feat: implement Category module with controller and service

---
+++ src/modules/category/category.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('category')
+export class CategoryController {
+    @Get()
+    findAll() {
+        return 'Category list   ';
+    }
+}
+
